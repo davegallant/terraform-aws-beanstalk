974f5e571f093b68f2b06698cd1aeddea16abd18
Dave Gallant <davegallant@gmail.com>
2021-03-21 21:55:23 -0400

    Fix terraform outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,10 @@
+output "app_version" {
+  value = aws_elastic_beanstalk_application_version.this.name
+}
 output "env_url" {
   value = "https://${aws_elastic_beanstalk_environment.this.endpoint_url}"
 }
 
-output "app_version" {
-  value = aws_elastic_beanstalk_application_version.this.name
+output "env_name" {
+  value = aws_elastic_beanstalk_environment.this.name
 }
